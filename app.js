b27bca9077a20a249e6dafe3e85cee3f1f4f6a46
camilabosio <cammibosio@gmail.com>
2022-03-15 19:03:37 -0300

    creando carpeta

---
+++ app.js
@@ -0,0 +1,10 @@
+//instación de prompt por medio de la terminal y luego se configura el código
+//const prompt = require("prompt-sync")({ sigint: true });
+//Probando su función en consola
+//let nombre = prompt("Ingrese su nombre: ");
+//console.log(`Hola ${nombre}`);
+
+
+Variables
+let primeraVariable = 'Hola';
+console.log(primeraVariable);
---
+++ datos.js
@@ -0,0 +1,10 @@
+
+//Tipos de datos
+//Number
+//String
+//Boolean
+
+let edad = 33;
+console.log(edad)
+
+let 
